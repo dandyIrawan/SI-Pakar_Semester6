2d6c1e1ab131006dd4d9751d0dd1b25d9005f57b
dandyirawan <ahmaddandiirawan@gmail.com>
2021-07-05 18:37:45 +0700

    login

--- salahlogin2.php
+++ salahlogin2.php
@@ -7,17 +7,23 @@
         <link href="css/login.css" rel="stylesheet" />
     </head>
     <body>
+    
             <div class="login-card">
             <h1>Log-in</h1><br>
+            <div><font color="red" size="3">
+        <a href="login.php" class="close">&times;</a>
+        Username dan Password salah </p>
+        </div> 
         <form role="form" method="post" action="ceklogin2.php">
             <input type="text" class="form-control" name="username" id="password"  placeholder="Username">
             <input type="password" class="form-control" name="password" id="password" placeholder="Password">
-            <input type="submit" name="login" class="login login-submit" value="login">
+            <input type="submit" name="login" class="login login-submit" value="Login">
         </form>
             
         <div class="login-help">
-            <a href="welcome.php">Kembali</a>
+            <a href="welcome.php">Kembali</a> 
         </div>
         </div>
+       
     </body>
     </html>
